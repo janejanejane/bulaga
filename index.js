99460443e39bafaf7b0656caec3d309942e045cb
janejanejane <jeanclaudetteambait@gmail.com>
2016-01-04 12:30:22 +1300

    Add upload image to s3 and delete in local storage

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require( 'dotenv' ).load();
 
 var spawn = require( 'child_process' ).spawn;
 var moment = require( 'moment' );
+var async = require( 'async' );
 var aws = require( 'aws-sdk' );
 
 var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
@@ -12,7 +13,8 @@ var S3_BUCKET = process.env.S3_BUCKET;
 var snapper = require( './snapper' );
 aws.config.update({
     accessKeyId: AWS_ACCESS_KEY,
-    secretAccessKey: AWS_SECRET_KEY
+    secretAccessKey: AWS_SECRET_KEY,
+    region: 'ap-northeast-1'
 });
 var s3 = new aws.S3();
 var params = {
@@ -24,9 +26,55 @@ var params = {
 // call spawn of child_process to execute snapshot command
 setInterval( function() {
     var filename = moment().format();
-    snapper.picture( spawn, filename );
-    params.Key = filename;
-    snapper.upload( s3, params );
+    var location = filename.split( 'T' );
+    params.Key = location[ 0 ] + '/' + location[ 1 ];
+
+    async.waterfall([
+	function takeSnapshot( callback ) {
+	    snapper.picture( spawn, filename, function( err, data ) {
+		if ( err ) {
+		    return callback( err );
+		}
+		callback( null, data );
+	    });
+	},
+	function readSavedSnapshot( status, callback ) {
+	    if ( status === 'created' ) {
+		snapper.read( filename, function( err, data ) {
+		    if ( err ) {
+		        return callback( err );
+		    }
+		    callback( null, data );	    
+		});
+	    } else {
+		return callback( status );
+	    }
+	},
+	function uploadSnapshotToS3( buffer, callback ) {
+	    params.Body = buffer;
+    	    snapper.upload( s3, params, function( err, data ) {
+	        if ( err ) {
+		    return callback( err );
+		}
+		callback( null, data );
+	    });
+	},
+	function deleteLocalSnapshot( uploadObj, callback ) {
+	    snapper.delete( filename, function( err, data ) {
+		if( err ) {
+		    return callback( err );
+		}
+		callback( null, 'done' );
+	    });
+	}
+    ], function ( err, result ) {
+	if ( err ) {
+	    console.log( 'waterfall error: ', err );
+	} else {
+	    console.log( 'waterfall result: ', result );
+	}
+    });
+
     console.log( 'inside interval' );
-}, 1000 );
+}, 1000);
 
--- snapper.js
+++ snapper.js
@@ -4,31 +4,50 @@ var fs = require( 'fs' );
 var fileLoc = './stream/image_stream';
 
 var snapper = {
-    picture: function( spawn, format ) {
+    picture: function( spawn, format, callback ) {
         var args = [ fileLoc + format + '.jpg' ];
 	var process = spawn( 'fswebcam', args );
 	process.on( 'close', function( data ) {
 	    console.log( 'close code', data );
+	    if ( data === 0 ) {
+		return callback( null, 'created' );
+	    } else {
+		return callback( 'error creation' );
+	    }
 	});
     },
-    upload: function( s3, params ) {
+    upload: function( s3, params, callback ) {
 	var self = this;
-	console.log( 'upload to s3' );
-	s3.getSignedUrl( 'putObject', params, function( err, data ) {
+	console.log( 'inside upload to s3' );
+	console.log( 'Params: ', params );
+	s3.putObject( params, function( err, data ) {
 	    if ( err ) {
 		console.log( 'Error uploading: ', err );
+		return callback( err );
 	    } else {
-		console.log( 'Successfully uploaded!' );
-		self.delete( params.Key );
+		console.log( 'Successfully uploaded!', data );
+		return callback( null, data );
 	    }
 	});
     },
-    delete: function( file ) {
-	fs.unlink( fileLoc + file + '.jpg', function( err ) {
+    read: function( filename, callback ) {
+	fs.readFile( fileLoc + filename + '.jpg', function( err, data ) {
+	    if ( err ) {
+		console.log( 'Error reading: ', err );
+		return callback( err );
+	    }
+	    console.log( 'Successfully read!' );
+	    return callback( null, data );
+	});
+    },
+    delete: function( filename, callback ) {
+	fs.unlink( fileLoc + filename + '.jpg', function( err ) {
 	    if ( err ) {
 	        console.log( 'Error deleting: ', err );
+		return callback( err );
 	    }
 	    console.log( 'Successfully deleted!' );
+	    return callback( null, 'done' );
 	});
     }
 };
