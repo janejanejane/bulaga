1dc7a7a965e8dc8c850d9054fd4e7b2a2dcc1f9f
janejanejane <jeanclaudetteambait@gmail.com>
2016-01-03 23:26:54 +1300

    Add snap loop

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 'use strict';
+require( 'dotenv' ).load();
 
 var spawn = require( 'child_process' ).spawn;
 var moment = require( 'moment' );
-var process;
+var aws = require( 'aws-sdk' );
+
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
 var snapper = require( './snapper' );
+aws.config.update({
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY
+});
+var s3 = new aws.S3();
+var params = {
+    Bucket: S3_BUCKET,
+    ContentType: 'image/jpeg',
+    ACL: 'public-read'
+};
 
-snapper.picture( spawn, process, moment().format() );
+// call spawn of child_process to execute snapshot command
+setInterval( function() {
+    var filename = moment().format();
+    snapper.picture( spawn, filename );
+    params.Key = filename;
+    snapper.upload( s3, params );
+    console.log( 'inside interval' );
+}, 1000 );
+
--- snapper.js
+++ snapper.js
@@ -1,9 +1,35 @@
 'use strict';
 
+var fs = require( 'fs' );
+var fileLoc = './stream/image_stream';
+
 var snapper = {
-    picture: function( spawn, process, format ) {
-        var args = [ './stream/image_stream' + format + '.jpg' ];
-        process = spawn( 'fswebcam', args );
+    picture: function( spawn, format ) {
+        var args = [ fileLoc + format + '.jpg' ];
+	var process = spawn( 'fswebcam', args );
+	process.on( 'close', function( data ) {
+	    console.log( 'close code', data );
+	});
+    },
+    upload: function( s3, params ) {
+	var self = this;
+	console.log( 'upload to s3' );
+	s3.getSignedUrl( 'putObject', params, function( err, data ) {
+	    if ( err ) {
+		console.log( 'Error uploading: ', err );
+	    } else {
+		console.log( 'Successfully uploaded!' );
+		self.delete( params.Key );
+	    }
+	});
+    },
+    delete: function( file ) {
+	fs.unlink( fileLoc + file + '.jpg', function( err ) {
+	    if ( err ) {
+	        console.log( 'Error deleting: ', err );
+	    }
+	    console.log( 'Successfully deleted!' );
+	});
     }
 };
 
